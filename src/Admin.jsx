266d201b96927f3045a81156458c7b21aa6d4a93
AlliMallika <allimallika19@gmail.com>
2024-09-10 12:03:42 +0530

    Component Creation & Call is done

---
+++ src/Admin.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const Admin = () => {
+  return (
+  <h1>This is Admin</h1>
+  )
+}
+
+// import React from "react";
+// const Admin =()=>{
+//     return(
+//         <h1>This is Admin</h1>
+//     )
+// }
+// export default Admin
--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,13 @@
 import React from "react";
+import Person from "./Person";
+
 const App = ()=>{
-  const name = 'Mallika';
-  const role = 'Backend Developer';
-  const Person = {
-    name:'Shiv',
-    place:'Kailash',
-  }
+ 
   return(
     // React Fragment added
     <>
-    <div>
-      <h1>Hi! This is {name}, {role}</h1>
-      <p>This is React Lecture.</p>
-    </div>
-    <div>
-      <h1>{Person.name}</h1>
-    </div>
+     <Person/>
+     {/* <h1>Hello world</h1> */}
     </>
   )
 }
