479271fa928e3505a27f7c44be0e96110d2e57d1
AlliMallika <allimallika19@gmail.com>
2024-09-10 10:38:19 +0530

    JSX done

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+const App = ()=>{
+  const name = 'Mallika';
+  const role = 'Backend Developer';
+  const Person = {
+    name:'Shiv',
+    place:'Kailash',
+  }
+  return(
+    <>
+    <div>
+      <h1>Hi! This is {name}, {role}</h1>
+      <p>This is React Lecture.</p>
+    </div>
+    <div>
+      <h1>{Person.name}</h1>
+    </div>
+    </>
+  )
+}
+export default App;
