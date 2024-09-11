1b32c11e2ca278b70e2d6b1fbd612066a3e8e53f
AlliMallika <allimallika19@gmail.com>
2024-09-11 14:45:29 +0530

    Events and useState done

--- src/App.jsx
+++ src/App.jsx
@@ -2,47 +2,71 @@ import React from "react";
 import Person from "./components/Person";
 import { Admin } from "./components/Admin";
 import { Product } from "./components/Product";
+import { Events } from "./components/Events";
+import { Counter } from "./components/Counter";
 
-const App = ()=>{
- //internal styling
-  const obj = {
-          display: 'flex', 
-          justifyContent: 'space-between', 
-          padding: '5px', 
-          margin: '2px'
-  }
+const App = () => {
+  // Internal styling for the button container
+  const buttonContainerStyle = {
+    display: 'flex',
+    justifyContent: 'center', // Center horizontally
+    alignItems: 'center',     // Center vertically
+    height: '100vh'           // Full viewport height
+  };
 
-  return(
-    // React Fragment added
+  // Base styling for product boxes
+  const baseProductStyle = {
+    padding: '10px',
+    margin: '20px',
+    display: 'flex',
+    justifyContent: 'space-between',
+    borderRadius: '10px',
+    border: '2px solid white',
+  };
+
+  const productStyles = {
+    gray: {
+      backgroundColor: 'gray',
+      ...baseProductStyle,
+    },
+    green: {
+      backgroundColor: 'green',
+      ...baseProductStyle,
+    },
+    blue: {
+      backgroundColor: 'blue',
+      ...baseProductStyle,
+    },
+  };
+
+  return (
     <>
-     <Person/>
-     {/* //resuable components */}
-     {/* <Admin/>  
-     <Admin/> */}
-       <h1><b><center><u>Different Types of Mobile Products</u></center></b></h1>
-     {/* //inline styling */}
-     <div style={{ 
+      {/* <Person/> */}
+      {/* <Admin/>  
+      <Admin/> */}
+      <h1><b><center><u>Different Types of Mobile Products</u></center></b></h1>
+      <div style={{ 
           display: 'flex', 
           justifyContent: 'space-between', 
           padding: '5px', 
           margin: '2px'
         }}>
-      {/* <div style={obj}> */}
-        {/* <div className='div'> //external CSS */}
-    <div style={{ backgroundColor:"gray", padding:'10px', margin:'20px', display:'flex',justifyContent:"space-between", borderRadius:"10px",
-          border:'2px solid white',}}>
-    <Product title="Galaxy s24 Ultra" brand="Samsung" price={150000} RAM="8GB"/>
-    </div>
-    <div style={{ backgroundColor:"green", padding:'10px', margin:'20px', display:'flex',justifyContent:"space-between", borderRadius:"10px",
-          border:'2px solid white',}}>
-      <Product title="IPhone 16" brand="Apple" price={750000}/>
-    </div>
-    <div style={{ backgroundColor:"blue", padding:'10px', margin:'20px', display:'flex',justifyContent:"space-between", borderRadius:"10px",
-          border:'2px solid white',}}>
-      <Product title="OnePlus13" brand="OnePlus" price={35000}/>
-    </div>
-    </div>
+        <div style={productStyles.gray}>
+          <Product title="Galaxy s24 Ultra" brand="Samsung" price={150000} RAM="8GB" />
+        </div>
+        <div style={productStyles.green}>
+          <Product title="IPhone 16" brand="Apple" price={750000} />
+        </div>
+        <div style={productStyles.blue}>
+          <Product title="OnePlus13" brand="OnePlus" price={35000} />
+        </div>
+      </div>
+      <div style={{textAlign:'center'}}>
+        <Events />
+      </div>
+      <div><Counter/></div>
     </>
-  )
+  );
 }
+
 export default App;
